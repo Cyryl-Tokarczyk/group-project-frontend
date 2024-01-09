664e23982f1c3dc569482ce69b6c0751dcd1cb31
Tokar <tokarczyk.cyryl2@gmail.com>
2024-01-09 20:54:48 +0100

    started handling of game flow, exported components from gameview to a separate folder

--- src/views/game/GameView.vue
+++ src/views/game/GameView.vue
@@ -1,16 +1,52 @@
 <script setup>
 import { onMounted } from 'vue';
-import EventComponent from './EventComponent.vue';
+import EventComponent from './components/EventComponent.vue';
 
 import { useSocketStore } from '@/stores/socket';
 
 const socketStore = useSocketStore()
 
+/* Flow of reacting:
+First message is popped on mount
+After handling a message another message is popped from the queue
+If there are no messages on the queue it should make a watch function watching a queue ???
+*/
+
 onMounted(() => {
   // Pop the first message
 
-  var serverMessage = socketStore.messsageQueue.pop()
+  handleMessage(socketStore.messsageQueue.pop())
 })
+
+function nextMessage() {
+  handleMessage(socketStore.messsageQueue.pop())
+}
+
+// TODO: Make an enum out of message types
+
+function handleMessage(message){
+  switch (message.type) {
+    case 'collect_action':
+      handleCollectAction(message)
+      break;
+    default:
+      break;
+  }
+
+  // Maybe just make a loop xd
+  nextMessage()
+}
+
+function handleCollectAction(message) {
+  // TODO: Handle the task
+  console.log(message);
+
+  // Send a response
+  socketStore.send({
+    type: 'collecting_move',
+    choice: ''
+  })
+}
 
 </script>
 
