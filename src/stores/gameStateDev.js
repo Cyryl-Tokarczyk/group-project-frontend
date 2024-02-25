657ed0f51b153bbf6c026999d797ae345cb32b4a
KacperPuda <125309126+KacperPuda@users.noreply.github.com>
2024-02-25 16:40:32 +0100

    rady button, undo button, better dynamic card position, other cards view, clash window

--- src/stores/gameStateDev.js
+++ src/stores/gameStateDev.js
@@ -44,8 +44,8 @@ export const useGameStateStore = defineStore('gameState', () => {
     throw new Error('Wrong/No playerType set')
   })
 
-  const actionCards = ref([{id: 1,des: "Karta A 1",color: "rgb(50,50,100)"},{id: 2,des: "Karta A 2",color: "rgb(100,50,0)"},{id: 3,des: "Karta A 3",color: "rgb(50,150,50)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"}])
-  const reactionCards = ref([{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"}])
+  const actionCards = ref([{id: 1,des: "Karta A 1",color: "rgb(50,50,100)"},{id: 2,des: "Karta A 2",color: "rgb(100,50,0)"},{id: 3,des: "Karta A 3",color: "rgb(50,150,50)"},{id: 1,des: "Karta A 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta A 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta A 3",color: "rgb(150,50,100)"}])
+  const reactionCards = ref([{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"},{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"}])
 
   function setPlayerMorale() {
     if (playerType.value == 'student') {
@@ -61,7 +61,7 @@ export const useGameStateStore = defineStore('gameState', () => {
   }
 
 
-  const opActionCards = ref([{id: 1,des: "Karta A 1",color: "rgb(50,150,120)"},{id: 2,des: "Karta A 2",color: "rgb(100,50,10)"},{id: 3,des: "Karta A 3",color: "rgb(50,150,100)"}])
+  const opActionCards = ref([{id: 1,des: "Karta A 1",color: "rgb(50,150,120)"}])
   const opRactionCards = ref([{id: 1,des: "Karta R 1",color: "rgb(150,210,100)"},{id: 2,des: "Karta R 2",color: "rgb(50,150,80)"},{id: 3,des: "Karta R 3",color: "rgb(150,50,100)"}])
 
   return { playerType, opponentType, money, morale, playersMorale, opponentsMorale, actionCards, reactionCards, setPlayerMorale, opActionCards, opRactionCards }
